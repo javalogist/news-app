a90a02a79ffde6ff91ae63bfe338b7df5deaa2df
vince.vijay <vince.vijay@rubicotech.in>
2023-03-16 10:26:24 +0530

    Initial commit

---
+++ app/src/main/java/com/developer/javalogist/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.developer.javalogist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.ui.Modifier
+import com.developer.javalogist.ui.theme.NewsAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            NewsAppTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
